bd8e931b0b55a2174b7b028eed3cb7c0311c601d
Mateo McAllister <mateomcallisterschool@gmail.com>
2025-02-05 21:43:29 -0500

    Update Lab3.py

--- m4/Lab3.py
+++ m4/Lab3.py
@@ -3,35 +3,36 @@ import math
 
 result = 0
 iterations = 0
+total_sum = 0
 
 while True:
     print(f'Current result: {result}\n\nCalculator Menu\n---------------\n0. Exit Program\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n5. Exponentiation\n6. Logarithm\n7. Display Average\n')
     selection = int(input('Enter Menu Selection: '))
-    if selection in range(1,8):
-        if selection == '0':
+    if selection in range(0,7):
+        if selection == 0:
             print('Thanks for using this calculator. Goodbye!')
             break
         op1 = input('Enter first operand: ')
         op2 = input('Enter second operand: ')
-        if selection == '1': # addition logic
+        if selection == 1: # addition logic
             if op1 == 'RESULT':
                 result = result + float(op2)
             elif op2 == 'RESULT':
                 result = result + float(op1)
             else: result = float(op1) + float(op2)
-        elif selection == '2': # subtraction logic
+        elif selection == 2: # subtraction logic
             if op1 == 'RESULT':
                 result = result - float(op2)
             elif op2 == 'RESULT':
                 result = result - float(op1)
             else: result = float(op1) - float(op2)
-        elif selection == '3': # multiplication logic
+        elif selection == 3: # multiplication logic
             if op1 == 'RESULT':
                 result = result * float(op2)
             elif op2 == 'RESULT':
                 result = result * float(op1)
             else: result = float(op1) * float(op2)
-        elif selection == '4': # division logic
+        elif selection == 4: # division logic
             if float(op2) == 0:
                 print('Error: invalid input!')
                 break
@@ -40,22 +41,25 @@ while True:
             elif op2 == 'RESULT':
                 result = result / float(op1)
             else: result = float(op1) / float(op2)
-        elif selection == '5': # exponentiation logic
+        elif selection == 5: # exponentiation logic
             if op1 == 'RESULT':
                 result = result ** float(op2)
             elif op2 == 'RESULT':
                 result = result ** float(op1)
             else: result = float(op1) ** float(op2)
-        elif selection == '6': # logarithm logic
+        elif selection == 6: # logarithm logic
             if op1 == 'RESULT':
                 result = math.log(float(op2), result)
             elif op2 == 'RESULT':
                 result = math.log(float(op1), result)
             else: result = math.log(float(op2), float(op1))
+    elif selection == 7: # averages logic
+        if iterations >= 1:
+            average = total_sum / iterations
+            print(f'Sum of calculations: {result}\nNumber of calculations: {iterations}\nAverage of calculations: {average:.2f}')
+            iterations -= 1
     else:
         print('Invalid selection.')
         break
+    total_sum += result
     iterations += 1
-
-
-
